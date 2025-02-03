d30ead814dfb22133dbf2b8143fb590fe54b7b49
Anirudh Gupta <gupta.anirudh@zomato.com>
2025-02-03 13:02:09 +0530

    refactor: Address PR feedback comments

--- sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/atoms/separator/SushiDividerProps.kt
+++ sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/atoms/separator/SushiDividerProps.kt
@@ -1,27 +1,32 @@
 package com.zomato.sushi.compose.atoms.separator
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.zomato.sushi.compose.atoms.color.ColorSpec
-import com.zomato.sushi.compose.atoms.color.asColorSpec
-import com.zomato.sushi.compose.foundation.ExperimentalSushiApi
-import com.zomato.sushi.compose.foundation.SushiUnspecified
 
-@ExperimentalSushiApi
 @Immutable
 data class SushiDividerProps(
-    val type: SushiDividerType? = Default.type,
-    val color: ColorSpec? = Default.color,
-    val isLeftIndented: Boolean? = Default.isLeftIndented,
-    val isRightIndented: Boolean? = Default.isLeftIndented,
-    val isBothIndented: Boolean? = Default.isLeftIndented
-) {
-    companion object {
-        val Default = SushiDividerProps(
-            type = SushiDividerType.Straight,
-            color = SushiUnspecified.asColorSpec(),
-            isLeftIndented = null,
-            isRightIndented = null,
-            isBothIndented = null
-        )
+    val type: SushiDividerType? = null,
+    val color: ColorSpec? = null,
+    val height: Dp? = null
+)
+
+object SushiDividerDefaults {
+    internal val type = SushiDividerType.Straight
+
+
+    fun getDividerHeight(type: SushiDividerType): Dp {
+        return when (type) {
+            SushiDividerType.Straight, SushiDividerType.Dashed -> 1f.dp
+            SushiDividerType.StraightThick -> 8f.dp
+            SushiDividerType.Dotted -> 3f.dp
+            SushiDividerType.DottedSpaced -> 0.8f.dp
+            SushiDividerType.Pink -> 3f.dp
+            SushiDividerType.Menu -> 2f.dp
+            is SushiDividerType.ZigZag -> type.height ?: 24f.dp
+            SushiDividerType.Vertical -> 1.dp
+            SushiDividerType.VerticalDotted -> 3.dp
+        }
     }
 }
