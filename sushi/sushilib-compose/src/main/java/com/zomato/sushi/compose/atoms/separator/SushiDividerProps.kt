7e29cb9bc6890ef067a4af89c5881e32abe32e85
Anirudh Gupta <gupta.anirudh@zomato.com>
2025-02-03 13:07:59 +0530

    refactor

--- sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/atoms/separator/SushiDividerProps.kt
+++ sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/atoms/separator/SushiDividerProps.kt
@@ -2,7 +2,6 @@ package com.zomato.sushi.compose.atoms.separator
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.zomato.sushi.compose.atoms.color.ColorSpec
 
 @Immutable
@@ -11,22 +10,3 @@ data class SushiDividerProps(
     val color: ColorSpec? = null,
     val height: Dp? = null
 )
-
-object SushiDividerDefaults {
-    internal val type = SushiDividerType.Straight
-
-
-    fun getDividerHeight(type: SushiDividerType): Dp {
-        return when (type) {
-            SushiDividerType.Straight, SushiDividerType.Dashed -> 1f.dp
-            SushiDividerType.StraightThick -> 8f.dp
-            SushiDividerType.Dotted -> 3f.dp
-            SushiDividerType.DottedSpaced -> 0.8f.dp
-            SushiDividerType.Pink -> 3f.dp
-            SushiDividerType.Menu -> 2f.dp
-            is SushiDividerType.ZigZag -> type.height ?: 24f.dp
-            SushiDividerType.Vertical -> 1.dp
-            SushiDividerType.VerticalDotted -> 3.dp
-        }
-    }
-}
