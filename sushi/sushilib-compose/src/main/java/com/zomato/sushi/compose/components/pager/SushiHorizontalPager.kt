210b6865d5342fcc90e0dd0928ce10b21917531d
Anirudh Gupta <gupta.anirudh@zomato.com>
2025-02-26 14:31:07 +0530

    feat: Implement Sushi pager and Z pager components

---
+++ sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/components/pager/SushiHorizontalPager.kt
@@ -0,0 +1,59 @@
+package com.zomato.sushi.compose.components.pager
+
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.TargetedFlingBehavior
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerScope
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * @author gupta.anirudh@zomato.com
+ */
+@Composable
+fun SushiHorizontalPager(
+    state: PagerState,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    pageSize: PageSize = PageSize.Fill,
+    beyondViewportPageCount: Int = PagerDefaults.BeyondViewportPageCount,
+    pageSpacing: Dp = 0.dp,
+    verticalAlignment: Alignment.Vertical = Alignment.CenterVertically,
+    flingBehavior: TargetedFlingBehavior = PagerDefaults.flingBehavior(state = state),
+    userScrollEnabled: Boolean = true,
+    reverseLayout: Boolean = false,
+    key: ((index: Int) -> Any)? = null,
+    pageNestedScrollConnection: NestedScrollConnection = PagerDefaults.pageNestedScrollConnection(
+        state,
+        Orientation.Horizontal
+    ),
+    snapPosition: SnapPosition = SnapPosition.Start,
+    pageContent: @Composable PagerScope.(page: Int) -> Unit
+) {
+    HorizontalPager(
+        state = state,
+        modifier = modifier.testTag("SushiHorizontalPager"),
+        contentPadding = contentPadding,
+        pageSize = pageSize,
+        beyondViewportPageCount = beyondViewportPageCount,
+        pageSpacing = pageSpacing,
+        verticalAlignment = verticalAlignment,
+        flingBehavior = flingBehavior,
+        userScrollEnabled = userScrollEnabled,
+        reverseLayout = reverseLayout,
+        key = key,
+        pageNestedScrollConnection = pageNestedScrollConnection,
+        snapPosition = snapPosition,
+        pageContent = pageContent
+    )
+}
