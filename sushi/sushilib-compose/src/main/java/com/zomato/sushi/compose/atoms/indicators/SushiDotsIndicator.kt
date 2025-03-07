9d2545490d9bd6a3146c26ac6e38bd654ca03077
Anirudh Gupta <gupta.anirudh@zomato.com>
2025-03-07 12:34:07 +0530

    remove lazyrow from indicators

--- sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/atoms/indicators/SushiDotsIndicator.kt
+++ sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/atoms/indicators/SushiDotsIndicator.kt
@@ -2,11 +2,9 @@ package com.zomato.sushi.compose.atoms.indicators
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.PagerState
@@ -26,7 +24,6 @@ import com.zomato.sushi.compose.atoms.indicators.type.BalloonIndicator
 import com.zomato.sushi.compose.atoms.indicators.type.ShiftIndicator
 import com.zomato.sushi.compose.atoms.indicators.type.SpringIndicator
 import com.zomato.sushi.compose.atoms.indicators.type.SushiIndicatorType
-import com.zomato.sushi.compose.atoms.indicators.type.WormIndicator
 import com.zomato.sushi.compose.atoms.internal.SushiComponentBase
 import com.zomato.sushi.compose.foundation.SushiTheme
 import com.zomato.sushi.compose.internal.SushiPreview
@@ -66,77 +63,53 @@ fun SushiDotsIndicator(
     onDotClicked: ((index: Int) -> Unit)? = null
 ) {
     SushiComponentBase(
-        Modifier
+        modifier
+            .height(IntrinsicSize.Max)
+            .width(IntrinsicSize.Max)
             .testTag("SushiDotsIndicator")
     ) {
-        val globalOffset by remember(dotCount, currentPage, currentPageOffsetFraction()) {
+        val currentPageOffset by remember(dotCount, currentPage, currentPageOffsetFraction()) {
             derivedStateOf {
-                computeGlobalScrollOffset(currentPage, currentPageOffsetFraction(), dotCount)
+                (currentPage + currentPageOffsetFraction()).coerceIn(0f, (dotCount - 1).toFloat())
             }
         }
 
         when (type) {
             is SushiIndicatorType.Balloon -> {
                 BalloonIndicator(
-                    globalOffsetProvider = { globalOffset },
+                    offsetProvider = { currentPageOffset },
                     dotCount = dotCount,
                     dotSpacing = dotSpacing,
                     onDotClicked = onDotClicked,
-                    modifier = modifier,
+                    modifier = Modifier.fillMaxSize(),
                     dotsGraphic = type.dotsGraphic,
                     balloonSizeFactor = type.balloonSizeFactor,
                 )
             }
             is SushiIndicatorType.Shift -> {
                 ShiftIndicator(
-                    globalOffsetProvider = { globalOffset },
+                    offsetProvider = { currentPageOffset },
                     dotCount = dotCount,
                     dotSpacing = dotSpacing,
                     onDotClicked = onDotClicked,
-                    modifier = modifier,
+                    modifier = Modifier,
                     dotsGraphic = type.dotsGraphic,
                     shiftSizeFactor = type.shiftSizeFactor,
                 )
             }
             is SushiIndicatorType.Spring -> {
                 SpringIndicator(
-                    globalOffsetProvider = { globalOffset },
+                    offsetProvider = { currentPageOffset },
                     dotCount = dotCount,
                     dotSpacing = dotSpacing,
                     onDotClicked = onDotClicked,
-                    modifier = modifier,
+                    modifier = Modifier.fillMaxSize(),
                     dotsGraphic = type.dotsGraphic,
                     selectorDotGraphic = type.selectorDotGraphic,
                 )
             }
-            is SushiIndicatorType.Worm -> {
-                WormIndicator(
-                    globalOffsetProvider = { globalOffset },
-                    dotCount = dotCount,
-                    dotSpacing = dotSpacing,
-                    onDotClicked = onDotClicked,
-                    modifier = modifier,
-                    dotsGraphic = type.dotsGraphic,
-                    wormDotGraphic = type.wormDotGraphic,
-                )
-            }
         }
     }
-}
-
-private fun computeGlobalScrollOffset(position: Int, positionOffset: Float, totalCount: Int): Float {
-    var offset = (position + positionOffset)
-    val lastPageIndex = (totalCount - 1).toFloat()
-    if (offset == lastPageIndex) {
-        offset = lastPageIndex - .0001f
-    }
-    val leftPosition = offset.toInt()
-    val rightPosition = leftPosition + 1
-    if (rightPosition > lastPageIndex || leftPosition < 0) {
-        return 0f
-    }
-
-    return leftPosition + offset % 1
 }
 
 @SushiPreview
@@ -263,56 +236,6 @@ private fun SushiDotsIndicatorSpringPreview() {
                 selectorDotGraphic = DotGraphic(
                     14.dp,
                     color = SushiTheme.colors.theme.v500.value
-                )
-            ),
-            currentPage = currentPage,
-            currentPageOffsetFraction = {
-                val value = animatedProgress.value
-                value - value.toInt()
-            }
-        )
-    }
-}
-
-
-@SushiPreview
-@Composable
-private fun SushiDotsIndicatorWormPreview() {
-    val dotCount = 5
-    val animatedProgress = remember { Animatable(0f) }
-    LaunchedEffect(Unit) {
-        while (true) {
-            delay(300)
-            val newValue = (animatedProgress.value + 1) % dotCount.toFloat()
-            if (newValue == 0f) {
-                animatedProgress.snapTo(newValue)
-            } else {
-                animatedProgress.animateTo(
-                    targetValue = (animatedProgress.value + 1) % dotCount.toFloat(),
-                    animationSpec = tween(durationMillis = 100, easing = LinearEasing)
-                )
-            }
-        }
-    }
-
-    val currentPage by remember {
-        derivedStateOf { animatedProgress.value.toInt() }
-    }
-
-    SushiPreview {
-        SushiDotsIndicator(
-            dotCount = dotCount,
-            dotSpacing = 8.dp,
-            type = SushiIndicatorType.Worm(
-                dotsGraphic = DotGraphic(
-                    16.dp,
-                    borderWidth = 2.dp,
-                    borderColor = SushiTheme.colors.theme.v500.value,
-                    color = Color.Transparent,
-                ),
-                wormDotGraphic = DotGraphic(
-                    16.dp,
-                    color = SushiTheme.colors.theme.v500.value,
                 )
             ),
             currentPage = currentPage,
--- sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/atoms/indicators/type/SpringIndicator.kt
+++ sushi/sushilib-compose/src/main/java/com/zomato/sushi/compose/atoms/indicators/type/SpringIndicator.kt
@@ -1,23 +1,25 @@
 package com.zomato.sushi.compose.atoms.indicators.type
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInParent
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.zomato.sushi.compose.atoms.indicators.Dot
 import com.zomato.sushi.compose.atoms.indicators.model.DotGraphic
 
 @Composable
 internal fun SpringIndicator(
-    globalOffsetProvider: () -> Float,
+    offsetProvider: () -> Float,
     dotCount: Int,
     dotSpacing: Dp,
     onDotClicked: ((Int) -> Unit)?,
@@ -25,74 +27,43 @@ internal fun SpringIndicator(
     dotsGraphic: DotGraphic = DotGraphic(),
     selectorDotGraphic: DotGraphic = DotGraphic(color = Color.Black),
 ) {
-    var firstDotPositionX: Float by remember(dotCount, dotsGraphic) { mutableStateOf(-1f) }
-    var lastDotPositionX: Float by remember(dotCount, dotsGraphic) { mutableStateOf(-1f) }
     Box(modifier = modifier) {
-        LazyRow(
-            modifier = Modifier
-                .fillMaxWidth(), content = {
-                items(dotCount) { dotIndex ->
-                    val dotModifier = when (dotIndex) {
-                        0 -> {
-                            Modifier.onGloballyPositioned {
-                                firstDotPositionX = it.positionInParent().x
-                            }
-                        }
-                        dotCount - 1 -> {
-                            Modifier.onGloballyPositioned {
-                                lastDotPositionX = it.positionInParent().x
-                            }
-                        }
-                        else -> Modifier
-                    }
-                    Dot(dotsGraphic, dotModifier.clickable {
-                        onDotClicked?.invoke(dotIndex)
-                    })
-                }
-            }, horizontalArrangement = Arrangement.spacedBy(
+        val percentage by remember(offsetProvider(), dotCount) {
+            derivedStateOf {
+                offsetProvider() / (dotCount - 1)
+            }
+        }
+        Row(
+            Modifier
+                .align(Alignment.Center)
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.spacedBy(
                 dotSpacing, alignment = Alignment.CenterHorizontally
             ),
-            contentPadding = PaddingValues(start = dotSpacing, end = dotSpacing)
-        )
-        if (firstDotPositionX != -1f && lastDotPositionX != -1f) {
-            val centeredOffset by remember {
-                derivedStateOf {
-                    (dotsGraphic.size - selectorDotGraphic.size) / 2
-                }
-            }
-            val density = LocalDensity.current.density
-            val foregroundDotPositionDp by remember(globalOffsetProvider) {
-                derivedStateOf {
-                    computeSelectorDotPositionDp(
-                        firstDotPositionX,
-                        lastDotPositionX,
-                        dotCount,
-                        globalOffsetProvider(),
-                        density,
-                        centeredOffset
-                    )
-                }
-            }
-            Dot(
-                selectorDotGraphic, Modifier.offset(
-                    x = foregroundDotPositionDp,
-                    y = centeredOffset
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            repeat(dotCount) { dotIndex ->
+                Dot(
+                    dotsGraphic,
+                    Modifier.clickable {
+                        onDotClicked?.invoke(dotIndex)
+                    }
                 )
-            )
+            }
         }
-    }
-}
+        Dot(
+            selectorDotGraphic,
+            Modifier
+                .align(Alignment.CenterStart)
+                .layout { measurable, constraints ->
+                    val placeable = measurable.measure(constraints)
+                    val parentWidth = constraints.maxWidth
+                    val offsetX = ((parentWidth - placeable.width) * percentage).toInt()
 
-private fun computeSelectorDotPositionDp(
-    firstDotPositionX: Float,
-    lastDotPositionX: Float,
-    dotCount: Int,
-    globalOffset: Float,
-    density: Float,
-    centeredOffset: Dp
-): Dp {
-    val foregroundDotPositionX =
-        firstDotPositionX + (lastDotPositionX - firstDotPositionX) / (dotCount - 1) * globalOffset
-    val foregroundDotPositionDp = (foregroundDotPositionX / density).dp
-    return foregroundDotPositionDp + centeredOffset
+                    layout(placeable.width, placeable.height) {
+                        placeable.place(offsetX, 0)
+                    }
+                }
+        )
+    }
 }
