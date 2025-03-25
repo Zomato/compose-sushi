57936c92367112a8dbfcd11044fe3ad2ef87b60f
Anirudh Gupta <gupta.anirudh@zomato.com>
2025-03-25 12:17:09 +0530

    migrate to kts, toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
         gradlePluginPortal()
         // todox: remove?
         maven {
-            url "https://maven.pkg.github.com/Zomato/*"
+            url = uri("https://maven.pkg.github.com/Zomato/*")
             credentials {
                 username = "Zomato"
                 password = System.getenv("READ_ARTIFACTS_TOKEN")
@@ -26,7 +26,7 @@ dependencyResolutionManagement {
         mavenCentral()
         // todox: remove?
         maven {
-            url "https://maven.pkg.github.com/Zomato/*"
+            url = uri("https://maven.pkg.github.com/Zomato/*")
             credentials {
                 username = "Zomato"
                 password = System.getenv("READ_ARTIFACTS_TOKEN")
@@ -35,25 +35,24 @@ dependencyResolutionManagement {
     }
 }
 
-def includeKits(String... kits) {
-    for (String kit : kits) {
-        def kitPath = "$kit/$kit"
-        def kitName = kit
+fun includeKits(vararg kits: String) {
+    for (kit in kits) {
+        var kitPath = "$kit/$kit"
+        var kitName = kit
         if (kit.contains('/')) {
             kitPath = kit
-            def splitData = kit.split('/')
-            kitName = splitData[splitData.length - 1]
+            val splitData = kit.split('/')
+            kitName = splitData[splitData.size - 1]
         }
-        include ":$kitName"
-        project(":$kitName").projectDir = new File(settingsDir, kitPath)
-
+        include(":$kitName")
+        project(":$kitName").projectDir = File(settingsDir, kitPath)
     }
 }
 
 rootProject.name = "Sushi Compose"
-include ':app'
+include(":app")
 
 includeKits(
-    'sushi/sushilib-core',
-    'sushi/sushilib-compose',
+    "sushi/sushilib-core",
+    "sushi/sushilib-compose",
 )
