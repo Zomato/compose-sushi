45c20ea25d661f3f58a737180a657a18f0b4ec3e
Anirudh Gupta <gupta.anirudh@zomato.com>
2025-03-26 15:48:42 +0530

    refactor: Reorganize folder structure

--- sushi/sushi-compose/build.gradle.kts
+++ sushi/sushi-compose/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.publish)
+    alias(libs.plugins.compose.screenshot)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.dokka)
 }
 
@@ -13,9 +15,8 @@ plugins {
 val getVersionInfoFunc = rootProject.extra["getVersionInfo"] as (String?) -> List<String>
 val versionInfo = getVersionInfoFunc(projectDir.absolutePath)
 val versionCode = versionInfo[0].toInt()
-val versionName = versionInfo[1].toString() // Ensure it's a String
+val versionName = versionInfo[1].toString()
 
-// Access the properties from root project
 val monoGroupId = rootProject.extra["monoGroupId"] as String
 val monoRepo = rootProject.extra["monoRepo"] as String
 
@@ -29,9 +30,11 @@ mavenPublishing {
 }
 
 android {
-    namespace = "com.zomato.sushi.core"
+    namespace = "com.zomato.sushi.compose"
 
     compileSdk = 35
+
+    experimentalProperties["android.experimental.enableScreenshotTest"] = true
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -40,8 +43,25 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
-    
+    api(project(":sushi-core"))
+
+    api(libs.lottie.compose)
+    api(libs.kotlinx.collections.immutable)
+
+    lintChecks(libs.slack.compose.lint)
+
+    // Compose dependencies
+    api(platform(libs.androidx.compose.bom))
+    api(libs.androidx.material3)
+
+    // Android Studio Preview support
+    api(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
 }
