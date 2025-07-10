675d32fc2c25ec819e46e5773bf451b338a8dd10
Anirudh Gupta <gupta.anirudh@zomato.com>
2025-07-10 16:23:04 +0530

    add docs for detectTapGestureWithoutConsuming

--- sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/gestures/TapGestureDetector.kt
+++ sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/gestures/TapGestureDetector.kt
@@ -6,6 +6,14 @@ import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.changedToUpIgnoreConsumed
 import kotlinx.coroutines.coroutineScope
 
+/**
+ * Detects tap gestures and calls [onTap] when detected.
+ * This does not consume any events, and [onTap] will be called even if the event has been consumed
+ * by someone else.
+ *
+ * After the initial press event, if the pointer moves out of the input area, the gestures are considered
+ * canceled. That means [onTap] will not be called after a gesture has been canceled.
+ */
 suspend fun PointerInputScope.detectTapGestureWithoutConsuming(
     onTap: () -> Unit
 ) = coroutineScope {
