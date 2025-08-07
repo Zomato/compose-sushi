b3a84331c8b813c8a69e663176054cc3659fddc5
Sudhanshu Tiwari <sudhanshu.tiwari@zomato.com>
2025-08-07 16:15:24 +0530

    train-v2

--- sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/atoms/loader/SushiLoaderProps.kt
+++ sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/atoms/loader/SushiLoaderProps.kt
@@ -24,5 +24,6 @@ data class SushiLoaderProps(
     val innerAngleOffset: Float? = null,
     val outerColor: ColorSpec = SushiUnspecified.asColorSpec(),
     val innerColor: ColorSpec = SushiUnspecified.asColorSpec(),
-    val animationSpeedMultiplier: Float? = null
+    val animationSpeedMultiplier: Float? = null,
+    val strokeWidth: Float? = null
 )
