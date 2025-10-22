e5941611ad41ef0f778c86de69213b519bf95c0f
Avishisht Gupta <avishisht.gupta@zomato.com>
2025-10-22 12:39:10 +0530

    feat: implement cross-platform window metrics retrieval

---
+++ sushi/sushi-compose/src/jsMain/kotlin/com/zomato/sushi/compose/utils/WindowMetrics.js.kt
@@ -0,0 +1,8 @@
+package com.zomato.sushi.compose.utils
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun rememberWindowMetrics(): WindowMetrics {
+    TODO("Not yet implemented")
+}
