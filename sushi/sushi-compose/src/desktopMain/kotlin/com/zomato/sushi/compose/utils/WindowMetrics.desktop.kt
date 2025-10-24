e56ad895aade96b7411baa38e30a2a5bab6e8604
Avishisht Gupta <avishisht.gupta@zomato.com>
2025-10-24 09:34:54 +0530

    feat: implement window metrics retrieval for desktop, JS, and wasm platforms

--- sushi/sushi-compose/src/desktopMain/kotlin/com/zomato/sushi/compose/utils/WindowMetrics.desktop.kt
+++ sushi/sushi-compose/src/desktopMain/kotlin/com/zomato/sushi/compose/utils/WindowMetrics.desktop.kt
@@ -1,8 +1,17 @@
 package com.zomato.sushi.compose.utils
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.window.rememberWindowState
 
 @Composable
 actual fun rememberWindowMetrics(): WindowMetrics {
-    TODO("Not yet implemented")
+    val windowState = rememberWindowState()
+
+    return remember(windowState) {
+        object : WindowMetrics {
+            override val windowWidthPx: Int = windowState.size.width.value.toInt()
+            override val windowHeightPx: Int = windowState.size.height.value.toInt()
+        }
+    }
 }
