4de1fcecee6fc8f83880672317f9a83c50eda1ad
Avishisht Gupta <avishisht.gupta@zomato.com>
2025-10-24 09:10:09 +0530

    refactor: remove underline button type and associated text decoration logic

--- sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/atoms/button/SushiButtonContentImpl.kt
+++ sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/atoms/button/SushiButtonContentImpl.kt
@@ -14,7 +14,6 @@ import com.zomato.sushi.compose.atoms.color.ColorSpec
 import com.zomato.sushi.compose.atoms.icon.SushiIcon
 import com.zomato.sushi.compose.atoms.icon.asIconSizeSpec
 import com.zomato.sushi.compose.atoms.text.SushiText
-import com.zomato.sushi.compose.atoms.text.SushiTextDecoration
 import com.zomato.sushi.compose.atoms.text.SushiTextProps
 import com.zomato.sushi.compose.atoms.text.asTextTypeSpec
 import com.zomato.sushi.compose.utils.takeIfSpecified
@@ -70,9 +69,7 @@ internal fun RowScope.SushiButtonContentImpl(
                     text = props.text,
                     color = appliedFontColor,
                     type = textType.asTextTypeSpec(),
-                    markdown = props.markdown,
-                    textDecoration = if (props.type?.equals(SushiButtonType.Underline) == true) SushiTextDecoration.Underline()
-                    else null
+                    markdown = props.markdown
                 )
             )
             if (!props.subText.isNullOrEmpty()) {
@@ -80,9 +77,7 @@ internal fun RowScope.SushiButtonContentImpl(
                     SushiTextProps(
                         text = props.subText,
                         color = appliedFontColor,
-                        type = with(SushiButtonDefaults) { getSubtextTextStyle(textType).asTextTypeSpec() },
-                        textDecoration = if (props.type?.equals(SushiButtonType.Underline) == true) SushiTextDecoration.Underline()
-                        else null
+                        type = with(SushiButtonDefaults) { getSubtextTextStyle(textType).asTextTypeSpec() }
                     )
                 )
             }
