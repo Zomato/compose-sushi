5220aada5b4bc66d1e45e57a69f983bb3197c5bd
Subhadeep Das <subhadeep.das@zomato.com>
2025-11-06 18:56:42 +0530

    sushi border

---
+++ sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/atoms/border/SushiBorderProps.kt
@@ -0,0 +1,16 @@
+package com.zomato.sushi.compose.atoms.border
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import com.zomato.sushi.compose.atoms.color.ColorSpec
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+
+@Immutable
+data class SushiBorderProps(
+    val colors: PersistentList<ColorSpec> = persistentListOf(),
+    val shape: Shape? = null,
+    val strokeColor: ColorSpec? = null,
+    val strokeWidth: Dp? = null
+)
