52a334f8cd319a7d49b9bea45e9f430caff3029b
Hitesh Raghuvanshi <hitesh.raghuvanshi@zomato.com>
2025-12-12 07:06:25 +0530

    Enhance media handling in Crystal components; replace ZImage with ZMedia for better resource management and logging improvements

--- sushi/sushi-compose/src/androidMain/kotlin/com/zomato/sushi/compose/atoms/animation/SushiAnimation.android.kt
+++ sushi/sushi-compose/src/androidMain/kotlin/com/zomato/sushi/compose/atoms/animation/SushiAnimation.android.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.compose.LottieAnimation
@@ -89,7 +90,8 @@ private fun LottieAutoPlay(
     LottieAnimation(
         composition = composition,
         progress = { animationState.progress },
-        modifier = modifier
+        modifier = modifier,
+        contentScale = ContentScale.Crop
     )
 }
 
