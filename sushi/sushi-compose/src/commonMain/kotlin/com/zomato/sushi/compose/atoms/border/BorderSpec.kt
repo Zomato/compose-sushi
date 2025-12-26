8ad7f19568f191c5f63d7483380a5bbc567f330c
Hitesh Raghuvanshi <hitesh.raghuvanshi@zomato.com>
2025-12-26 13:49:46 +0530

    add: introduce BorderSpec and update references in snippet files

---
+++ sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/atoms/border/BorderSpec.kt
@@ -0,0 +1,21 @@
+package com.zomato.sushi.compose.atoms.border
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.Dp
+import com.zomato.sushi.compose.atoms.color.SushiGradientColorSpec
+import com.zomato.sushi.compose.utils.BorderType
+
+data class BorderSpec(
+    val width: Dp,
+    val color: SushiGradientColorSpec? = null,
+    val borderType: BorderType? = null,
+    val borderSides: BorderSides? = BorderSides(),
+    val shape: RoundedCornerShape? = null
+)
+
+data class BorderSides(
+    val left: Boolean = true,
+    val top: Boolean = true,
+    val right: Boolean = true,
+    val bottom: Boolean = true
+)
