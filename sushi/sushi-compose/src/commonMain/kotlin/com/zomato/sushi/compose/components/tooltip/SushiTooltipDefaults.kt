d1b3b3da9c4a2f08e42fd3ab0048cc43cf1eee8a
Avishisht Gupta <avishisht.gupta@zomato.com>
2026-03-13 17:12:19 +0530

    updated noCaretShape to use RoundedCornerShape

--- sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/components/tooltip/SushiTooltipDefaults.kt
+++ sushi/sushi-compose/src/commonMain/kotlin/com/zomato/sushi/compose/components/tooltip/SushiTooltipDefaults.kt
@@ -5,34 +5,23 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TooltipAnchorPosition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupPositionProvider
 import com.zomato.sushi.compose.components.tooltip.base.DefaultTooltipCaretShape
 import com.zomato.sushi.compose.components.tooltip.base.TooltipPositionProviderImpl
-
-private object NoCaretShape : Shape {
-    override fun createOutline(
-        size: androidx.compose.ui.geometry.Size,
-        layoutDirection: LayoutDirection,
-        density: Density,
-    ) = Outline.Rectangle(Rect.Zero)
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 object SushiTooltipDefaults {
 
     fun caretShape() = DefaultCaretShape
 
-    val noCaretShape: Shape = NoCaretShape
+    val noCaretShape: Shape = RoundedCornerShape(12.dp)
 
     val caretSize: DpSize = DpSize(16.dp, 8.dp)
 
